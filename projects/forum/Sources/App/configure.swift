66fef53ff72326c15a4187dc80a49444fddedb5a
yun.ao <yun.ao@grabtaxi.com>
2019-01-02 19:03:05 +0800

    remove file unused

--- projects/forum/Sources/App/configure.swift
+++ projects/forum/Sources/App/configure.swift
@@ -1,10 +1,11 @@
-import FluentSQLite
+import FluentMySQL
 import Vapor
+import Fluent
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register providers first
-    try services.register(FluentSQLiteProvider())
+    try services.register(FluentMySQLProvider())
 
     /// Register routes to the router
     let router = EngineRouter.default()
@@ -17,17 +18,24 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
-
-    /// Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+
+    let mysqlConfig = MySQLDatabaseConfig(
+            hostname: "mysql",
+            port: 3306,
+            username: "vapor",
+            password: "vapor",
+            database: "vapor",
+            transport: .unverifiedTLS)
+
+    let mysqlDB = MySQLDatabase(config: mysqlConfig)
+
+    databases.add(database: mysqlDB, as: .mysql)
     services.register(databases)
 
     /// Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
+//    migrations.add(model: Todo.self, database: .sqlite)
     services.register(migrations)
 
 }
