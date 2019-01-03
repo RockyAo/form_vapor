178ea10c2d1f98ba045079e736716864e063044d
yun.ao <yun.ao@grabtaxi.com>
2019-01-03 18:45:36 +0800

    first route

--- projects/forum/Sources/App/routes.swift
+++ projects/forum/Sources/App/routes.swift
@@ -1,14 +1,29 @@
 import Vapor
 
+struct UserContext: Codable, Content {
+    var username: String?
+    var forums: [Forum]
+}
+
+func getUsername() -> String?{
+    return "rocky"
+}
+
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
         return "It works!"
     }
-    
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+
+    router.get("forums") {
+        req -> Future<Response> in
+
+        return Forum.query(on: req)
+                .all()
+                .map(to: UserContext.self) { forums -> UserContext in
+                    return UserContext(username: getUsername(), forums: forums)
+                }
+                .encode(status: .created, for: req)
     }
 }
