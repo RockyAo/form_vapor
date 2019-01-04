6d1d917d5e106abe56a763a82f3c975e835492b1
yun.ao <yun.ao@grabtaxi.com>
2019-01-04 14:44:55 +0800

    get forums route

--- projects/forum/Sources/App/routes.swift
+++ projects/forum/Sources/App/routes.swift
@@ -1,8 +1,16 @@
 import Vapor
+import Fluent
+import FluentMySQL
 
 struct UserContext: Codable, Content {
     var username: String?
     var forums: [Forum]
+}
+
+struct MessageContext: Codable, Content {
+    var username: String?
+    var forum: Forum
+    var messages: [Message]
 }
 
 func getUsername() -> String?{
@@ -25,5 +33,31 @@ public func routes(_ router: Router) throws {
                     return UserContext(username: getUsername(), forums: forums)
                 }
                 .encode(status: .created, for: req)
+    }
+
+    router.group("forums",Int.parameter) {
+        group in
+
+        group.get("messages") {
+            req -> Future<Response> in
+            let forumId = try req.parameters.next(Int.self)
+
+            return Forum.find(forumId, on: req).flatMap(to: Response.self) {
+                forum in
+                guard let forum = forum else { throw Abort(.notFound) }
+
+                return Message.query(on: req)
+                        .filter(\.forumID == forum.id!)
+                        .filter(\.originID == 0)
+                        .all()
+                        .map {
+                            return MessageContext(
+                                    username: "bx11",
+                                    forum: forum,
+                                    messages: $0)
+                        }
+                        .encode(status: .ok, for: req)
+            }
+        }
     }
 }
